75f714e241c02d3a5465a48705db50fc6dac42d4
magnum-z <zr260578jea@gmail.com>
2018-01-08 01:25:23 +0200

    Added project - p0181

---
+++ p0181dynamiclayout3/src/main/java/ru/startandroid/p0181/MainActivity.java
@@ -0,0 +1,58 @@
+package ru.startandroid.p0181;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+
+import static android.widget.SeekBar.*;
+
+public class MainActivity extends AppCompatActivity implements OnSeekBarChangeListener {
+
+    SeekBar sbWeight;
+    Button btn1;
+    Button btn2;
+
+    LinearLayout.LayoutParams lParams1;
+    LinearLayout.LayoutParams lParams2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sbWeight = findViewById(R.id.sbWeight);
+        sbWeight.setOnSeekBarChangeListener(this);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        lParams1 = (LinearLayout.LayoutParams) btn1.getLayoutParams();
+        lParams2 = (LinearLayout.LayoutParams) btn2.getLayoutParams();
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        int leftValue = i;
+        int rightValue = seekBar.getMax() - i;
+        lParams1.weight = leftValue;
+        lParams2.weight = rightValue;
+
+        btn1.setText(String.valueOf(leftValue));
+        btn2.setText(String.valueOf(rightValue));
+        // it is not necessary because setText invokes requestLayout
+        // btn1.requestLayout();
+        // btn2.requestLayout();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
