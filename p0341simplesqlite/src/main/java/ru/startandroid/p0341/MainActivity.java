f362daf2ea44ce2c14798294cf7733ea07e5a79f
magnum-z <zr260578jea@gmail.com>
2018-02-07 18:10:08 +0200

    Added project - p0341

---
+++ p0341simplesqlite/src/main/java/ru/startandroid/p0341/MainActivity.java
@@ -0,0 +1,130 @@
+package ru.startandroid.p0341;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    final String LOG_TAG = "myLogs";
+
+    Button btnAdd, btnRead, btnClear;
+    EditText etName, etEmail;
+
+    DBHelper dbHelper;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
+
+        btnRead = findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(this);
+
+        btnClear = findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+
+        // create object for managing DB
+        dbHelper = new DBHelper(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // create object for data
+        ContentValues cv = new ContentValues();
+
+        // get data from EditText widgets
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+
+        // connect to DB
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                Log.d(LOG_TAG, "--- Insert in mytable: ---");
+
+                // preparing data (pairs of "column of table - values") for save
+                cv.put("name", name);
+                cv.put("email", email);
+
+                // insert record and get record's ID
+                long rowID = db.insert("mytable", null, cv);
+                Log.d(LOG_TAG, "row inserted, ID = " + rowID);
+                break;
+            case R.id.btnRead:
+                Log.d(LOG_TAG, "--- Rows in mytable: ---");
+
+                // select all records from table "mytable" to Cursor object
+                Cursor c = db.query("mytable", null, null, null, null, null, null);
+
+                // move position of cursor on first record
+                // return false if cursor is empty
+                if (c.moveToFirst()) {
+                    // get index of column in cursor
+                    int idColIndex = c.getColumnIndex("id");
+                    int nameColIndex = c.getColumnIndex("name");
+                    int emailColIndex = c.getColumnIndex("email");
+
+                    do {
+                        // get value and write to log
+                        Log.d(LOG_TAG,
+                                "ID = " + c.getInt(idColIndex) +
+                                        ", name = " + c.getString(nameColIndex) +
+                                        ", email = " + c.getString(emailColIndex));
+                        // move position on next record
+                        // false if record is last
+                    } while (c.moveToNext());
+                } else
+                    Log.d(LOG_TAG, "0 rows");
+                c.close();
+                break;
+            case R.id.btnClear:
+                Log.d(LOG_TAG, "--- Clear mytable: ---");
+                // delete all records
+                int clearCount = db.delete("mytable", null, null);
+                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+                break;
+        }
+        // close connection to DB
+        dbHelper.close();
+    }
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context) {
+            super(context, "myDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+            // create table
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "name text,"
+                    + "email text" + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+
+}
