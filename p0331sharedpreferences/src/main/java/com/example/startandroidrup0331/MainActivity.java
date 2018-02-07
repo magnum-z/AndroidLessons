c19683741d541f70ec75716c9f84ce0e00eca282
magnum-z <zr260578jea@gmail.com>
2018-02-07 17:15:55 +0200

    Added project - p0331

---
+++ p0331sharedpreferences/src/main/java/com/example/startandroidrup0331/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.startandroidrup0331;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    EditText etText;
+    Button btnSave;
+    Button btnLoad;
+
+    SharedPreferences sPref;
+
+    final String SAVED_TEXT = "saved_text";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etText = findViewById(R.id.etText);
+
+        btnSave = findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+
+        btnLoad = findViewById(R.id.btnLoad);
+        btnLoad.setOnClickListener(this);
+
+        loadText();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveText();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSave:
+                saveText();
+                saveText_WorldReadable();
+                saveText_WorldWritable();
+                break;
+            case R.id.btnLoad:
+                loadText();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void saveText() {
+        sPref = getPreferences(MODE_PRIVATE);
+        Editor ed = sPref.edit();
+        ed.putString(SAVED_TEXT, etText.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
+    }
+
+    void loadText() {
+        sPref = getPreferences(MODE_PRIVATE);
+        String savedText = sPref.getString(SAVED_TEXT, "");
+        etText.setText(savedText);
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
+    }
+
+    void saveText_WorldReadable() {
+        sPref = getSharedPreferences("textWR", MODE_WORLD_READABLE);
+        Editor ed = sPref.edit();
+        ed.putString(SAVED_TEXT, etText.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
+    }
+
+    void saveText_WorldWritable() {
+        sPref = getSharedPreferences("textWW", MODE_WORLD_WRITEABLE);
+        Editor ed = sPref.edit();
+        ed.putString(SAVED_TEXT, etText.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
+    }
+
+
+}
