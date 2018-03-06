5487fff56a6b36dfbd40ba1b4cf9ec8346bcf355
magnum-z <zr260578jea@gmail.com>
2018-03-06 18:26:08 +0200

    Added project - p0371

---
+++ p0371sqliteinnerjoin/src/main/java/ru/startandroid/p0371/MainActivity.java
@@ -0,0 +1,141 @@
+package ru.startandroid.p0371;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    // data for table of positions
+    int[] position_id = { 1, 2, 3, 4 };
+    String[] position_name = { "Boss", "Programmer", "Accountant", "Security" };
+    int[] position_salary = { 15000, 13000, 10000, 8000 };
+
+    // data for table of people
+    String[] people_name = { "Ivan", "Maria", "Petr", "Anton", "Dasha", "Boris", "Kostya", "Igor" };
+    int[] people_posid = { 2, 3, 2, 2, 3, 1, 2, 4 };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // connect to DB
+        DBHelper dbh = new DBHelper(this);
+        SQLiteDatabase db = dbh.getWritableDatabase();
+
+        Cursor c;
+
+        // print to log data of positions
+        Log.d(LOG_TAG, "--- Table position ---");
+        c = db.query("position", null, null, null, null, null, null);
+        logCursor(c);
+        c.close();
+        Log.d(LOG_TAG, "--- ---");
+
+        // print to log data of people
+        Log.d(LOG_TAG, "--- Table people ---");
+        c = db.query("people", null, null, null, null, null, null);
+        logCursor(c);
+        c.close();
+        Log.d(LOG_TAG, "--- ---");
+
+        // print result of join
+        // using rawQuery
+        Log.d(LOG_TAG, "--- INNER JOIN with rawQuery---");
+        String sqlQuery = "select PL.name as Name, PS.name as Position, salary as Salary "
+                + "from people as PL "
+                + "inner join position as PS "
+                + "on PL.posid = PS.id "
+                + "where salary > ?";
+        c = db.rawQuery(sqlQuery, new String[] {"12000"});
+        logCursor(c);
+        c.close();
+        Log.d(LOG_TAG, "--- ---");
+
+        // print result of join
+        // using query
+        Log.d(LOG_TAG, "--- INNER JOIN with query---");
+        String table = "people as PL inner join position as PS on PL.posid = PS.id";
+        String columns[] = { "PL.name as Name", "PS.name as Position", "salary as Salary" };
+        String selection = "salary < ?";
+        String[] selectionArgs = {"12000"};
+        c = db.query(table, columns, selection, selectionArgs, null, null, null);
+        logCursor(c);
+        c.close();
+        Log.d(LOG_TAG, "--- ---");
+
+        // close connection to DB
+        dbh.close();
+    }
+
+    // print to log data from cursor
+    void logCursor(Cursor c) {
+        if (c != null) {
+            if (c.moveToFirst()) {
+                String str;
+                do {
+                    str = "";
+                    for (String cn : c.getColumnNames()) {
+                        str = str.concat(cn + " = " + c.getString(c.getColumnIndex(cn)) + "; ");
+                    }
+                    Log.d(LOG_TAG, str);
+                } while (c.moveToNext());
+            }
+        } else
+            Log.d(LOG_TAG, "Cursor is null");
+    }
+
+    // helper class for operations with DB
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context) {
+            super(context, "myDB", null, 1);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+
+            ContentValues cv = new ContentValues();
+
+            // create table of positions
+            db.execSQL("create table position ("
+                    + "id integer primary key,"
+                    + "name text," + "salary integer"
+                    + ");");
+
+            for (int i = 0; i < position_id.length; i++) {
+                cv.clear();
+                cv.put("id", position_id[i]);
+                cv.put("name", position_name[i]);
+                cv.put("salary", position_salary[i]);
+                db.insert("position", null, cv);
+            }
+
+            // create table of people
+            db.execSQL("create table people ("
+                    + "id integer primary key autoincrement,"
+                    + "name text,"
+                    + "posid integer"
+                    + ");");
+
+            for (int i = 0; i < people_name.length; i++) {
+                cv.clear();
+                cv.put("name", people_name[i]);
+                cv.put("posid", people_posid[i]);
+                db.insert("people", null, cv);
+            }
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+}
